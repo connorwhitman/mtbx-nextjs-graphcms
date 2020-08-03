4b832e7ea62659a6971b265414d6fdc111241e38
Connor Whitman <cwhitman@s7designs.com>
2020-08-03 13:42:48 -0400

    Add default props

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,23 +1,29 @@
-import React from 'react'
-import { FooterWrap } from './footer.stc'
-
-type FooterProps = {
-  copy?: string,
-}
+import React from 'react';
+import { FooterWrap } from './footer.stc';
 
 const Footer = ({ copy }: FooterProps) => {
   return (
     <FooterWrap>
-        <a
-          href="https://connorwhitman.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {copy}
-          <img src="/logo.svg" alt="connorwhitman.com Logo" />
-        </a>
-      </FooterWrap>
+      <a
+        href="https://connorwhitman.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {copy}
+        <img src="/logo.svg" alt="connorwhitman.com Logo" />
+      </a>
+    </FooterWrap>
   );
 };
 
-export default Footer
+type FooterProps = {
+  copy?: string;
+};
+
+const defaultProps: FooterProps = {
+  copy: 'otherDefault',
+};
+
+Footer.defaultProps = defaultProps;
+
+export default Footer;
